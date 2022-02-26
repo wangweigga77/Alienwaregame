8b49038f70cc1093eb45bfdf8f289c1ca5f7712c
wangwei <wangwei@example.com>
2022-02-26 16:55:59 +0800

    增加了飞船类文件,images文件及飞船图,并在主游戏类中引用飞船类绘制飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -11,6 +12,7 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -20,8 +22,10 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
-            #  每次循环都重绘屏幕并设背景色
+            #  每次循环都重绘屏幕
             self.screen.fill(self.settings.bg_color)
+            # 填充背景后，调用blitme()将飞船绘制在屏幕上，确保它出现在背景前面
+            self.ship.blitme()
 
             #  让最后一次绘制的屏幕可见
             pygame.display.flip()
