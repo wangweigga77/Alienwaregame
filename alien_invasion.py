7e3947ddb698af9e8b2ec4498906f176243e0ab5
wangwei <wangwei@example.com>
2022-03-02 23:37:13 +0800

    修改游戏主程序的__init__()的屏幕设置,在全屏模式下运行游戏

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,7 +10,9 @@ class AlienInvasion:
         """初始化游戏并创建游戏资源"""
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
 
