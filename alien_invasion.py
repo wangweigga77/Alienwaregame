a5f00e01c4d5062f9378e4e855cd458a9141d908
wangwei <wangwei@example.com>
2022-03-07 00:16:35 +0800

    重构_update_bullets()方法使得游戏主循环看起来更加简洁

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,13 +23,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            # 对编组调用update()时，编组自动其中的每个精灵调用bullet.update()
-            self.bullets.update()
-            # 删除消失的子弹,因为for循环遍历列表时，Python要求该列表的长度在整个循环中保持不变，
-            # 所以不能从for循环遍历的列表中删除元素，所以必须遍历编组的副本。
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -75,6 +69,16 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """更新子弹位置,并将超出屏幕的子弹删除"""
+        # 对编组调用update()时，编组自动其中的每个精灵调用bullet.update()
+        self.bullets.update()
+        # 删除消失的子弹,因为for循环遍历列表时，Python要求该列表的长度在整个循环中保持不变，
+        # 所以不能从for循环遍历的列表中删除元素，所以必须遍历编组的副本。
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
                         
     def _update_screen(self):
         """更新屏幕绘制的内容并刷新在屏幕上"""
